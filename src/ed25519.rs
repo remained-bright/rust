71ff0fb58038f8c920ea0043e43812ed769ad3b5
gcxfd <i@rmw.link>
2021-06-24 17:44:04 +0800

    🔶

--- src/ed25519.rs
+++ src/ed25519.rs
@@ -3,34 +3,19 @@ use rand_core::{OsRng, RngCore};
 use std::mem::MaybeUninit;
 use std::time::Instant;
 
-struct ArrIncr<const N: usize> {
-  pos: usize,
-  pub arr: [u8; N],
-}
-
-impl<const N: usize> ArrIncr<N> {
-  pub fn new() -> Self {
-    let mut arr: [u8; N] = unsafe { MaybeUninit::uninit().assume_init() };
-    (OsRng {}).fill_bytes(&mut arr[..]);
-    ArrIncr { pos: 0, arr: arr }
-  }
-}
-
-impl<const N: usize> Iterator for ArrIncr<N> {
-  type Item = [u8; N];
-  fn next(&mut self) -> Option<[u8; N]> {
-    let pos = self.pos;
-    self.arr[pos] = u8::wrapping_add(self.arr[pos], 1);
-    self.pos = (pos + 1) % self.arr.len();
-    Some(self.arr)
-  }
+pub fn rand<const N: usize>() -> [u8; N] {
+  let mut arr: [u8; N] = unsafe { MaybeUninit::uninit().assume_init() };
+  (OsRng {}).fill_bytes(&mut arr[..]);
+  arr
 }
 
 pub fn seed() {
   let now = Instant::now();
   let mut n = 0;
 
-  for seed in ArrIncr::<32>::new() {
+  loop {
+    let seed = rand::<32>();
+
     let secret = SecretKey::from_bytes(&seed).unwrap();
     let public: PublicKey = (&secret).into();
 
@@ -39,7 +24,7 @@ pub fn seed() {
 
     n += 1;
     if n % 10000 == 0 {
-      println!("{} : public {:?}", n, public.as_bytes());
+      println!("{} : public {:?}", n / 10000, public.as_bytes());
     }
     let bytes = public.as_bytes();
     if bytes[0] == 0 && bytes[1] == 0 {
