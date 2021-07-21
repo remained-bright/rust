ff16a6ad901de3e5bcaf28152dedbb052e11078d
gcxfd <i@rmw.link>
2021-07-21 18:11:18 +0800

    🔶

--- src/udp/mod.rs
+++ src/udp/mod.rs
@@ -3,11 +3,19 @@ mod timer;
 use crate::udp::recv_from::{recv_from, CONNECTED_TIME};
 use crate::udp::timer::timer;
 use crate::util::now;
+use crate::var::msl::MSL;
 use anyhow::Result;
 use async_std::net::UdpSocket;
 use log::error;
 use retainer::Cache;
+use static_init::dynamic;
 use std::time::Duration;
+
+#[dynamic]
+static DURATION: u64 = 3;
+
+#[dynamic]
+static EXPIRE: u64 = *DURATION + (*MSL).as_secs() + 1;
 
 pub async fn listen(addr: String) -> Result<()> {
   let connecting = Cache::<[u8; 6], ()>::new();
@@ -16,11 +24,11 @@ pub async fn listen(addr: String) -> Result<()> {
   let err = futures::join!(
     timer(&socket, &connecting),
     recv_from(&socket, &connecting),
-    connecting.monitor(2, 1, Duration::from_secs(3), &|kvli| {
-      if kvli.len() > 0 && now::sec() - unsafe { CONNECTED_TIME } <= 6 {
-        //6秒内有过成功的连接
+    connecting.monitor(2, 1, Duration::from_secs(*DURATION), &|kvli| {
+      //msl秒内有过成功的连接
+      if kvli.len() > 0 && (now::sec() - unsafe { CONNECTED_TIME }) <= *EXPIRE {
         for (k, v) in kvli {
-          println!("{:?} {:?}", k, v)
+          println!("TODO REMOVE {:?} {:?}", k, v)
         }
       }
     }),
--- src/var/msl.rs
+++ src/var/msl.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 #[dynamic]
 // MSL (Maximum Segment Lifetime) : 报文段最大生存时间，这里用作连接超时时间
 pub static MSL: Duration = {
-  let mut msl = config_get!(msl, { 30.to_string() }).parse().unwrap();
+  let mut msl = config_get!(msl, { 6.to_string() }).parse().unwrap();
   if msl < 1 {
     msl = 1
   }
