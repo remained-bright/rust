8373963667eaa79557a831cac5addfbabb89d502
gcxfd <i@rmw.link>
2021-07-22 13:12:28 +0800

    🔶

--- src/util/find_port.rs
+++ src/util/find_port.rs
@@ -1,8 +1,8 @@
 use rand::{thread_rng, Rng};
-use std::net::UdpSocket;
+use std::net::{Ipv4Addr, UdpSocket};
 
 pub fn port_available(port: u16) -> bool {
-  match UdpSocket::bind(("0.0.0.0", port)) {
+  match UdpSocket::bind((Ipv4Addr::UNSPECIFIED, port)) {
     Ok(_) => true,
     Err(_) => false,
   }
@@ -10,7 +10,7 @@ pub fn port_available(port: u16) -> bool {
 
 pub fn find_port() -> u16 {
   let mut rng = thread_rng();
-  let p: u16 = rng.gen_range(8081..20000);
+  let p: u16 = rng.gen_range(9001..10000);
   (p..65535)
     .find(|port| port_available(*port))
     .expect("no udp port available")
