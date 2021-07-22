ddfbcaa0404862b5a47c0553d34d98447d5be210
gcxfd <i@rmw.link>
2021-07-22 17:11:15 +0800

    🔶

--- src/kad.rs
+++ src/kad.rs
@@ -15,8 +15,8 @@ struct Kad {
 impl Kad {
   pub fn add(node: Node) -> bool {
     let mut distance = 0;
-    if distance > 64 {
-      distance = 64;
+    if distance > 32 {
+      distance = 32;
     }
     false
   }
