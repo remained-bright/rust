3f14575585516e127a404d492fc4634d77bc9d61
gcxfd <i@rmw.link>
2021-07-22 14:59:34 +0800

    🔶

--- src/udp/upnp.rs
+++ src/udp/upnp.rs
@@ -2,10 +2,10 @@ use async_std::task::sleep;
 use igd::aio::search_gateway;
 use log::info;
 use std::net::TcpStream;
-use std::net::{IpAddr, SocketAddrV4};
+use std::net::{IpAddr, Ipv4Addr, SocketAddrV4};
 use std::time::Duration;
 
-pub async fn upnp(name: &str, port: u16, duration: u32) -> bool {
+pub async fn upnp(name: &str, port: u16, duration: u32) -> Option<Ipv4Addr> {
   if let Ok(gateway) = search_gateway(Default::default()).await {
     if let Ok(stream) = TcpStream::connect(gateway.addr) {
       if let Ok(addr) = stream.local_addr() {
@@ -24,21 +24,29 @@ pub async fn upnp(name: &str, port: u16, duration: u32) -> bool {
           {
             info!("upnp failed : {}", err);
           } else {
-            return true;
+            return Some(ip);
           }
         }
       }
     }
   }
-  false
+  None
 }
 
 const SLEEP_SECONDS: u32 = 60;
 
 pub async fn upnp_daemon(name: &str, port: u16) {
+  let mut local_ip = Ipv4Addr::UNSPECIFIED;
+
   let seconds = Duration::from_secs(SLEEP_SECONDS.into());
+
   loop {
-    upnp(name, port, SLEEP_SECONDS + 60).await;
+    if let Some(ip) = upnp(name, port, SLEEP_SECONDS + 60).await {
+      if ip != local_ip {
+        local_ip = ip;
+        info!("upnp success ( {}:{} )", local_ip, port);
+      }
+    };
     sleep(seconds).await;
   }
 }
