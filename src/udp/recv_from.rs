08850de06f27321d8b82afb54eee7cbcad5ee587
gcxfd <i@rmw.link>
2021-07-27 13:53:02 +0800

    🔶

--- src/udp/recv_from.rs
+++ src/udp/recv_from.rs
@@ -84,7 +84,7 @@ pub async fn recv_from(socket: &UdpSocket, connecting: &Cache<[u8; 6], ()>) -> R
                 }
                 CMD::KEY => {
                   let key = &input[1..n];
-                  println!("key.len {}", key.len());
+                  println!("key.len {} n = {}", key.len(), n);
                   if key.len() == 30 {
                     reply!(
                       CMD::Q,
