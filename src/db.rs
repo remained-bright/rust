44dd3b1827d1f79ebdef5b582cc04f4ca28ac1ac
gcxfd <i@rmw.link>
2021-07-28 11:26:21 +0800

    🔶

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use crate::args::DIR;
 use crate::util::now;
 use anyhow::Result;
-pub use persy::{ByteVec, Config, Persy, ValueMode};
+pub use persy::{Config, Persy, ValueMode};
 use static_init::dynamic;
 use std::path::{Path, PathBuf};
 
@@ -20,8 +20,8 @@ pub mod db {
 pub static TX: Persy = {
   Persy::open_or_create_with(&*DB_FILE, Config::new(), |p| {
     let mut tx = p.begin()?;
-    tx.create_index::<ByteVec, u64>(db::ipv4_time, ValueMode::Replace)?;
-    tx.create_index::<u64, ByteVec>(db::time_ipv4, ValueMode::Cluster)?;
+    tx.create_index::<[u8; 6], u64>(db::ipv4_time, ValueMode::Replace)?;
+    tx.create_index::<u64, [u8; 6]>(db::time_ipv4, ValueMode::Cluster)?;
     //tx.create_segment(db::ipv4)?;
     tx.commit()?;
     Ok(())
@@ -30,7 +30,6 @@ pub static TX: Persy = {
 };
 
 pub fn ipv4_insert(addr: [u8; 6]) -> Result<bool> {
-  let addr = ByteVec(addr.to_vec());
   let mut now = now::sec();
   let mut tx = TX.begin()?;
 
@@ -39,10 +38,10 @@ pub fn ipv4_insert(addr: [u8; 6]) -> Result<bool> {
       return Ok(false);
     }
     now = u64::min(v >> 1, now);
-    tx.remove(db::time_ipv4, v, Some(addr.clone()))?;
+    tx.remove(db::time_ipv4, v, Some(addr))?;
   }
 
-  tx.put(db::ipv4_time, addr.clone(), now)?;
+  tx.put(db::ipv4_time, addr, now)?;
   tx.put(db::time_ipv4, now, addr)?;
 
   tx.commit()?;
@@ -54,17 +53,16 @@ const MAX_TIME: u64 = (!0) >> 1;
 pub fn ipv4_offline(addr: [u8; 6]) -> Result<()> {
   let mut tx = TX.begin()?;
   let mut time: u64 = MAX_TIME;
-  let addr = ByteVec(addr.to_vec());
 
   for t in tx.get::<_, u64>(db::ipv4_time, &addr)? {
     time = t;
-    tx.remove(db::time_ipv4, time, Some(addr.clone()))?;
+    tx.remove(db::time_ipv4, time, Some(addr))?;
   }
   if time >= MAX_TIME {
     tx.remove::<_, u64>(db::ipv4_time, addr, None)?;
   } else {
     time = time << 1;
-    tx.put(db::time_ipv4, time, addr.clone())?;
+    tx.put(db::time_ipv4, time, addr)?;
     tx.put(db::ipv4_time, addr, time)?;
   }
 
